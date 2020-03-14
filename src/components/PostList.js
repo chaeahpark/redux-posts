48b005fc4eedb10155da3facca1a9757b01f8eaf
chaeahpark <parkchaeah331@gmail.com>
2020-03-14 16:52:42 +0200

    set up reducers and actions

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class PostList extends Component {
+  componentDidMount() {}
+
+  render() {
+    return <div>PostList</div>;
+  }
+}
+
+export default PostList;
